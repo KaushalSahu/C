6b259e7cdb64b3fb347cbfa1099c7a3e67db31f6
KaushalSahu <kaushal.raj.sahu@gmail.com>
2018-10-10 18:53:28 +0530

    Assignment

---
+++ BBST/q1.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+	int data;
+	struct node *left,*right;
+};
+struct node *newNode(int data)
+{
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->data = data;
+	temp->left = NULL;
+	temp->right = NULL;
+	return temp;
+}
+struct node  *insert(struct node *root,int x)
+{
+	if(root == NULL)
+		return newNode(x);
+	if(x<root->data)
+		root->left = insert(root->left,x);
+	if(x>root->data)
+		root->right = insert(root->right,x);
+
+	return root;
+}
+void inorder(struct node *root)
+{
+	if(root!=NULL)
+	{
+		inorder(root->left);
+		printf("%d ",root->data);
+		inorder(root->right);
+	}
+}
+void preorder(struct node *root)
+{
+	if(root!=NULL)
+	{
+		printf("%d ",root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+struct node *rightrotate(struct node *root)
+{
+	struct node *temp;
+	struct node *temp2 = root->left;
+	if(root!=NULL)
+	{
+		if(root->left!=NULL && root->right!=NULL)
+		{
+			if(root->left->right != NULL)
+				temp = root->left->right;
+			root->left->right = root;
+			root->left= temp;
+		}
+	}
+	return temp2;
+}
+
+int main()
+{
+	int i;
+	int n;
+	int x;
+	struct node *root = NULL;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&x);
+		root = insert(root,x);
+	}
+	inorder(root);
+	printf("\n");
+	root = rightrotate(root);
+	preorder(root);
+	printf("\n");
+	return 0;
+}
