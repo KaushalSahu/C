c6685edaee4c35ff6f83983ca27019c95709fab7
KaushalSahu <kaushal.raj.sahu@gmail.com>
2018-10-19 21:54:55 +0530

    Assignments

--- 2D Arrays/q4.c
+++ 2D Arrays/q4.c
@@ -8,6 +8,7 @@ int main()
 	for(i = 0;i<n;i++)
 		for(j=0;j<n;j++)
 			scanf("%d",&a[i][j]);
+
 	
 	return 0;
 }
---
+++ 2D Arrays/q5.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+#define n1 puts("")
+void sort(char (*names)[],int n)
+{
+	int i;
+	int j;
+	char (*temp)[30]=names;
+	char key[30];
+	for(i=0;i<n;i++)
+	{
+		strcpy(key,temp[i]);
+		j = i-1;
+		while(strcmp(key,temp[j])<0 && j>=0)
+		{
+			strcpy(temp[j+1],temp[j]);
+			j--;
+		}
+		strcpy(temp[j+1],key);
+	}
+}
+int main()
+{
+	int n;
+	int i;
+	scanf("%d",&n);
+	char names[n][30];
+	char prev[30];
+	for(i=0;i<n;i++)
+		scanf("%s",names[i]);
+	sort(names,n);
+	puts("Sorted :");
+	for(i=0;i<n;i++)
+		printf("%s ",names[i]);
+	n1;
+	puts("Unique:");
+	for(i=0;i<n;i++)
+	{
+		if(i == 0)
+			printf("%s ",names[i]);
+		else if(strcmp(prev,names[i])!=0)
+			printf("%s ",names[i]);
+		strcpy(prev,names[i]);
+	}
+	n1;
+	return 0;
+}
